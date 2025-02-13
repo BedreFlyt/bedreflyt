6c78542b20a715a48d1d57741bd1d1a0e91efc1c
Paul Kobialka <p.kobialka@web.de>
2025-02-13 16:45:25 +0000

    added run file

---
+++ z3/run.sh
@@ -0,0 +1,6 @@
+python3 genetic_instances_copy.py --cores 30 --generations 100
+python3 genetic_instances_copy.py --cores 30 --generations 100 --patient_names 40
+python3 genetic_instances_copy.py --cores 30 --generations 100 --patient_names 60
+python3 genetic_instances_copy.py --cores 30 --generations 100
+python3 genetic_instances_copy.py --cores 30 --generations 100 --patient_day 15
+python3 genetic_instances_copy.py --cores 30 --generations 100 --patient_day 5
